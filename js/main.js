fa6b4133ae4559d93bb42a00eeb89d4885fe1068
Christina Paik <christinapaik@gmail.com>
2017-11-04 10:43:43 -0400

    add id tags to form and add eventListener for form input fields

--- js/main.js
+++ js/main.js
@@ -3,5 +3,18 @@ var heading = document.getElementById('mainHeader');
 heading.addEventListener('click', function (event) {
     var myElement = document.createElement('article');
     myElement.innerHTML = 'This is click number ' + event.detail;
-    document.getElementsByClassName('container')[0].appendChild(myElement);
+    document.getElementsByClassName('wrapper')[0].appendChild(myElement);
+});
+
+var firstName = document.getElementById('firstName');
+var lastName = document.getElementById('lastName');
+var email = document.getElementById('email_address');
+var message = document.getElementById('message');
+
+document.querySelector('form#contact_form').addEventListener('submit', function (eventInfo) {
+    eventInfo.preventDefault();
+    console.log('The firstName is: ' + firstName.value);
+    console.log('The lastName is: ' + lastName.value);
+    console.log('The email is: ' + email.value);
+    console.log('The message is: ' + message.value);
 });
